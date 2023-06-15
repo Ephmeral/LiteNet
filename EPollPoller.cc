9485ee1971c730c1f68de860fc20613fcb10e9a1
Ephmeral <2669128599@qq.com>
2023-06-15 20:04:35 +0800

    Add EPollPoller class, inherit Poller and implement epoll function

---
+++ EPollPoller.cc
@@ -0,0 +1,114 @@
+#include "EPollPoller.h"
+#include "Logger.h"
+
+#include <cstring>
+#include <unistd.h>
+
+// channel未添加到Poller中
+const int kNew = -1;  // channel的成员index_ = -1
+// channel已添加到Poller中
+const int kAdded = 1;
+// channel从Poller中删除
+const int kDeleted = 2;
+
+EPollPoller::EPollPoller(EventLoop *loop)
+    : Poller(loop), epollfd_(::epoll_create1(EPOLL_CLOEXEC)), events_(kInitEventListSize) {
+    if (epollfd_ < 0) {
+        LOG_FATAL("epoll_create error:%d \n", errno);
+    }
+}
+
+EPollPoller::~EPollPoller() {
+    ::close(epollfd_);
+}
+
+Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels) {
+    LOG_INFO("func=%s => fd total count:%lu\n", __FUNCTION__, channels_.size());
+    
+    int numEvents = ::epoll_wait(epollfd_, &(*events_.begin()), static_cast<int>(events_.size()), timeoutMs);
+    int saveErrno = errno;
+
+    Timestamp now(Timestamp::now());
+    
+    if (numEvents > 0) {
+        LOG_INFO("%d events happened \n", numEvents);
+        fillActiveChannels(numEvents, activeChannels);
+        if (numEvents == events_.size()) {
+            events_.resize(events_.size() * 2);
+        }
+    } else if (numEvents == 0) {
+        LOG_DEBUG("%s timeout!\n", __FUNCTION__);
+    } else {
+        if (saveErrno != EINTR) {
+            errno = saveErrno;
+            LOG_ERROR("EPollerPoller::poll() error!");
+        }
+    }
+    return now;
+}
+
+void EPollPoller::updateChannel(Channel *channel) {
+    const int index = channel->index();
+    LOG_INFO("func=%s => fd=%d events=%d index=%d \n", __FUNCTION__, channel->fd(), channel->events(), index);
+
+    if (index == kNew || index == kDeleted) {
+        if (index == kNew) {
+            int fd = channel->fd();
+            channels_[fd] = channel;
+        }
+        channel->set_index(kAdded);
+        update(EPOLL_CTL_ADD, channel);
+    } else {
+        // channel已经在Poller上注册过了
+        int fd = channel->fd();
+        if (channel->isNoneEvent()) {
+            update(EPOLL_CTL_DEL, channel);
+            channel->set_index(kDeleted);
+        } else {
+            update(EPOLL_CTL_MOD, channel);
+        }
+    }
+}
+
+void EPollPoller::removeChannel(Channel *channel) {
+    int fd = channel->fd();
+    channels_.erase(fd);
+
+    LOG_INFO("func=%s => fd=%d \n", __FUNCTION__, channel->fd());
+
+    int index = channel->index();
+    if (index == kAdded) {
+        update(EPOLL_CTL_DEL, channel);
+    }
+    channel->set_index(kNew);
+}
+
+// 填写活跃的连接
+void EPollPoller::fillActiveChannels(int numEvents, ChannelList *activeChannels) {
+    for (int i = 0; i < numEvents; ++i) {
+        Channel* channel = static_cast<Channel*>(events_[i].data.ptr);
+        channel->set_events(events_[i].events);
+        // EventLoop拿到它的poller给它返回的所有发生事件的channel列表了
+        activeChannels->push_back(channel); 
+    }
+}
+
+// 更新Channel通道
+void EPollPoller::update(int operation, Channel *channel) {
+    epoll_event event;
+    memset(&event, 0, sizeof(event));
+
+    int fd = channel->fd();
+
+    event.events = channel->events();
+    event.data.ptr = channel;
+    event.data.fd = fd;
+
+    if (::epoll_ctl(epollfd_, operation, fd, &event) < 0) {
+        if (operation == EPOLL_CTL_DEL) {
+            LOG_ERROR("epoll_ctr del error:%d\n", errno);
+        } else {
+            LOG_FATAL("epoll_ctl add/mod/del error:%d\n", errno)
+        }
+    }
+}
---
+++ EPollPoller.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <sys/epoll.h>
+#include <vector>
+
+#include "Poller.h"
+#include "Timestamp.h"
+#include "Channel.h"
+
+class EPollPoller : public Poller {
+public:
+    EPollPoller(EventLoop *loop);
+    ~EPollPoller() override;
+
+    // 重写基类Poller的抽象方法
+    Timestamp poll(int timeoutMs, ChannelList *activeChannels_) override;
+    void updateChannel(Channel *channel) override;
+    void removeChannel(Channel *channel) override;
+
+private:
+    static const int kInitEventListSize = 16;
+
+    // 填写活跃的连接
+    void fillActiveChannels(int numEvents, ChannelList *activeChannels_);
+    // 更新Channel通道
+    void update(int operation, Channel *channel);
+
+    using EventList = std::vector<epoll_event>;
+
+    int epollfd_;
+    EventList events_;
+};
