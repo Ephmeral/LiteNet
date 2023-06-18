04af7a55eb4972f43c312f9a24eb416023008b44
Ephmeral <2669128599@qq.com>
2023-06-18 16:37:43 +0800

    Add Socket class, wrap the raw socket in linux

---
+++ Socket.cc
@@ -0,0 +1,66 @@
+#include "Socket.h"
+
+#include <netinet/tcp.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "InetAddress.h"
+#include "Logger.h"
+
+Socket::~Socket() {
+    close(sockfd_);
+}
+
+void Socket::bindAddress(const InetAddress& localaddr) {
+    if (0 != ::bind(sockfd_, (sockaddr*)localaddr.getSockAddr(), sizeof(sockaddr_in))) {
+        LOG_FATAL("bind sockfd:%d failed \n", sockfd_);
+    }
+}
+
+void Socket::listen() {
+    if (0 != ::listen(sockfd_, 1024)) {
+        LOG_FATAL("listen sockfd:%d failed \n", sockfd_);
+    }
+}
+
+int Socket::accept(InetAddress* peeraddr) {
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof(addr));
+    socklen_t len;
+    int connfd = ::accept(sockfd_, (sockaddr*)&addr, &len);
+    if (connfd >= 0) {
+        peeraddr->setSockAddr(addr);
+    }
+    return connfd;
+}
+
+void Socket::shutdownWrite() {
+    if (::shutdown(sockfd_, SHUT_WR) < 0) {
+        LOG_ERROR("shutdown Write error");
+    }
+}
+
+void Socket::setTcpNoDelay(bool on) {
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, IPPROTO_TCP, TCP_NODELAY,
+                 &optval, static_cast<socklen_t>(sizeof(optval)));
+}
+
+void Socket::setReuseAddr(bool on) {
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR,
+                 &optval, static_cast<socklen_t>(sizeof(optval)));
+}
+
+void Socket::setReusePort(bool on) {
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_REUSEADDR,
+                 &optval, static_cast<socklen_t>(sizeof(optval)));
+}
+void Socket::setKeepAlive(bool on) {
+    int optval = on ? 1 : 0;
+    ::setsockopt(sockfd_, SOL_SOCKET, SO_KEEPALIVE,
+                 &optval, static_cast<socklen_t>(sizeof(optval)));
+}
