a3009cba5f55c6bd3445ef6bbf281424ea217a72
Ephmeral <2669128599@qq.com>
2023-06-22 13:15:03 +0800

    Adjust: LOG information

--- EPollPoller.cc
+++ EPollPoller.cc
@@ -3,6 +3,7 @@
 
 #include <cstring>
 #include <unistd.h>
+#include <cassert>
 
 // channel未添加到Poller中
 const int kNew = -1;  // channel的成员index_ = -1
@@ -14,7 +15,7 @@ const int kDeleted = 2;
 EPollPoller::EPollPoller(EventLoop *loop)
     : Poller(loop), epollfd_(::epoll_create1(EPOLL_CLOEXEC)), events_(kInitEventListSize) {
     if (epollfd_ < 0) {
-        LOG_FATAL("epoll_create error:%d \n", errno);
+        LOG_FATAL("EPollPoller::epoll_create error:%d \n", errno);
     }
 }
 
@@ -22,8 +23,8 @@ EPollPoller::~EPollPoller() {
     ::close(epollfd_);
 }
 
-Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels) {
-    LOG_INFO("func=%s => fd total count:%lu\n", __FUNCTION__, channels_.size());
+Timestamp EPollPoller::poll(int timeoutMs, ChannelList &activeChannels) {
+    LOG_INFO("EPollPoller::%s() => fd total count:%lu\n", __FUNCTION__, channels_.size());
     
     int numEvents = ::epoll_wait(epollfd_, &(*events_.begin()), static_cast<int>(events_.size()), timeoutMs);
     int saveErrno = errno;
@@ -31,13 +32,13 @@ Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels) {
     Timestamp now(Timestamp::now());
     
     if (numEvents > 0) {
-        LOG_INFO("%d events happened \n", numEvents);
+        LOG_INFO("EPollPoller::poll() %d events happened \n", numEvents);
         fillActiveChannels(numEvents, activeChannels);
         if (numEvents == events_.size()) {
             events_.resize(events_.size() * 2);
         }
     } else if (numEvents == 0) {
-        LOG_DEBUG("%s timeout!\n", __FUNCTION__);
+        LOG_DEBUG("EPollPoller::%s timeout!\n", __FUNCTION__);
     } else {
         if (saveErrno != EINTR) {
             errno = saveErrno;
@@ -49,7 +50,7 @@ Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels) {
 
 void EPollPoller::updateChannel(Channel *channel) {
     const int index = channel->index();
-    LOG_INFO("func=%s => fd=%d events=%d index=%d \n", __FUNCTION__, channel->fd(), channel->events(), index);
+    LOG_INFO("EPollPoller::%s => fd=%d events=%d index=%d \n", __FUNCTION__, channel->fd(), channel->events(), index);
 
     if (index == kNew || index == kDeleted) {
         if (index == kNew) {
@@ -74,7 +75,7 @@ void EPollPoller::removeChannel(Channel *channel) {
     int fd = channel->fd();
     channels_.erase(fd);
 
-    LOG_INFO("func=%s => fd=%d \n", __FUNCTION__, channel->fd());
+    LOG_INFO("EPollPoller::%s => fd=%d \n", __FUNCTION__, channel->fd());
 
     int index = channel->index();
     if (index == kAdded) {
@@ -84,31 +85,34 @@ void EPollPoller::removeChannel(Channel *channel) {
 }
 
 // 填写活跃的连接
-void EPollPoller::fillActiveChannels(int numEvents, ChannelList *activeChannels) {
+void EPollPoller::fillActiveChannels(int numEvents, ChannelList &activeChannels) {
+    assert(static_cast<size_t>(numEvents) <= events_.size());
     for (int i = 0; i < numEvents; ++i) {
         Channel* channel = static_cast<Channel*>(events_[i].data.ptr);
-        channel->set_events(events_[i].events);
+        assert(channel != nullptr);
+        channel->set_revents(events_[i].events);
         // EventLoop拿到它的poller给它返回的所有发生事件的channel列表了
-        activeChannels->push_back(channel); 
+        activeChannels.push_back(channel); 
     }
 }
 
 // 更新Channel通道
 void EPollPoller::update(int operation, Channel *channel) {
+    LOG_INFO("EPoller::update() chennel");
     epoll_event event;
     memset(&event, 0, sizeof(event));
 
     int fd = channel->fd();
 
     event.events = channel->events();
-    event.data.ptr = channel;
     event.data.fd = fd;
+    event.data.ptr = channel;
 
     if (::epoll_ctl(epollfd_, operation, fd, &event) < 0) {
         if (operation == EPOLL_CTL_DEL) {
-            LOG_ERROR("epoll_ctr del error:%d\n", errno);
+            LOG_ERROR("EPollPoller::epoll_ctr del error:%d\n", errno);
         } else {
-            LOG_FATAL("epoll_ctl add/mod/del error:%d\n", errno)
+            LOG_FATAL("EPollPoller::epoll_ctl add/mod/del error:%d\n", errno)
         }
     }
 }
